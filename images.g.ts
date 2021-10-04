590cbf39bafc779635a8ae0d44fd1f80e677b262
Ckyiu <UnsignedArduino@outlook.com>
2021-10-04 08:41:31 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,29 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "life_image":return img`
+. . . . . . . 
+. . 3 3 3 . . 
+. . . . . 3 . 
+. 3 . . 3 3 3 
+3 3 3 . . 3 . 
+. 3 . . . . . 
+. . 3 3 3 . . 
+. . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
---
+++ main.ts
@@ -0,0 +1,6 @@
+function prepare_game () {
+    info.setLife(2500)
+    spriteutils.setLifeImage(assets.image`life_image`)
+    info.setScore(0)
+}
+prepare_game()
